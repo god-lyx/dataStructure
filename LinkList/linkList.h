53927265871ae3d998e612fcc2174c8797b61729
Desjajja <58029489+Desjajja@users.noreply.github.com>
2020-11-24 16:36:53 +0800

    初步实现非循环单链表

---
+++ LinkList/linkList.h
@@ -0,0 +1,10 @@
+#define OK 1
+#define ERROR 0
+typedef int Status;
+#include <cstddef>
+
+template<class T>
+class LinkList;//链表
+
+template<class T>
+struct Node;//链表结点
---
+++ LinkList/main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include"linkList.cpp"
+using namespace std;
+
+int main(){
+    int option, i, e, output;
+    LinkList<int> L;
+    while(1){
+    cout<<"**************单链表测试**************"<<endl;
+    cout<<"【1】取非循环单链表的第i个结点；"<<endl;
+    cout<<"【2】在第i个结点前插入数据域为e的结点"<<endl;
+
+    cout<<"请输入操作码：";
+    cin>>option;
+
+    switch(option){
+        case 1:
+        cout<<"请输入取出元素的位序：";
+        cin>>i;
+
+        if(output = L(i))
+            cout<<"第i个结点的数据域为："<<output<<endl;
+        else cout<<"操作失败！"<<endl;
+        continue;
+
+        case 2:
+        cout<<"输入插入位序：";
+        cin>>i;
+        cout<<"输入数据域：";
+        cin>>e;
+
+        if(!L.insertNode(i, e))cout<<"操作失败！"<<endl;
+        continue;
+    }
+}
+}
